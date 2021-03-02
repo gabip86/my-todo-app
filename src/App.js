7ec00209daeb8e4b5d02b590131aca056b9b0bae
Gabi <gabee_88@yahoo.com>
2021-03-02 14:04:03 +0100

    feat: created App.js and some todo related methods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import { Button, Card, Form } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      todo: 'This is my first todo',
+      isDone: false
+    }
+  ])
+
+  const addTodo = todo => {
+    const newTodos = [...todos, { todo }]
+    setTodos(newTodos)
+  }
+
+  const markTodo = index => {
+    const newTodos = [...todos]
+    newTodos[index].isDone = true
+    setTodos(newTodos)
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">Todo List</h1>
+        <FormTodo addTodo={addTodo} />
+        <div>
+          {todos.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <Todo
+                  key={index}
+                  index={index}
+                  todo={todo}
+                  markTodo={markTodo}
+                  removeTodo={removeTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
